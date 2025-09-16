71c0430691b9020c7b1776cd7f273ce6f4cbad85
AjithkumarPS24 <ajithkumar.ps@reach24.net>
2025-09-16 16:28:48 +0530

    Final refactored version

--- lib/string_calculator.rb
+++ lib/string_calculator.rb
@@ -2,18 +2,25 @@ class StringCalculator
   def add(numbers_string)
     return 0 if numbers_string.strip.empty?
 
+    numbers = parse_numbers(numbers_string)
+    validate_no_negatives(numbers)
+    numbers.sum
+  end
+
+  private
+
+  def parse_numbers(numbers_string)
     if numbers_string.start_with?("//")
-      delimiter, numbers_string = numbers_string[2..].split("\n", 2)
-      parts = numbers_string.split(delimiter)
+      delimiter, numbers_part = numbers_string[2..].split("\n", 2)
+      parts = numbers_part.split(delimiter)
     else
       parts = numbers_string.split(/[\s,]+/)
     end
+    parts.map(&:to_i)
+  end
 
-    numbers = parts.map(&:to_i)
-
+  def validate_no_negatives(numbers)
     negatives = numbers.select { |n| n < 0 }
     raise ArgumentError, "negative numbers not allowed: #{negatives.join(',')}" if negatives.any?
-
-    numbers.sum
   end
 end
