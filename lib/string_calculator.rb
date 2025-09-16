9d191d2ddfb9f56cf21ce5ef73d45c0501bfe2e0
AjithkumarPS24 <ajithkumar.ps@reach24.net>
2025-09-16 14:27:22 +0530

    Added test case for sum of all positive numbers

--- lib/string_calculator.rb
+++ lib/string_calculator.rb
@@ -1,5 +1,7 @@
 class StringCalculator
   def add(numbers_string)
     return 0 if numbers_string.strip.empty?
+
+    numbers_string.split(",").map(&:to_i).sum
   end
 end
--- spec/string_calculator_spec.rb
+++ spec/string_calculator_spec.rb
@@ -2,14 +2,18 @@ require './lib/string_calculator'
 
 describe StringCalculator do
   describe '#add' do
+    let(:string_calculator) { StringCalculator.new }
+
     it 'returns zero for empty string' do
-      string_calculator = StringCalculator.new
       expect(string_calculator.add("")).to eql(0)
     end
 
     it 'returns zero for non-empty string (whitespace)' do
-      string_calculator = StringCalculator.new
       expect(string_calculator.add(" ")).to eql(0)
+    end
+
+    it 'returns sum of all positive numbers' do
+      expect(string_calculator.add("5, 3, 6")).to eql(14)
     end
   end
 end
